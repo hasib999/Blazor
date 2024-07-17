c1e5e032cc61e71e6943250267f1bdd310a1ff10
Md. Hasibul Islam <45915540+hasib999@users.noreply.github.com>
2024-07-17 15:29:11 +0600

    update net 8

---
+++ BlazorCrudAppDotNet8/Models/ApplicationDBContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BlazorCrudAppDotNet8.Models
+{
+    public class ApplicationDBContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=HASIBSHANTO;Database=BlazorDb;Trusted_Connection=True;TrustServerCertificate=True;MultipleActiveResultSets=True");
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ BlazorCrudAppDotNet8/Models/Product.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorCrudAppDotNet8.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Type { get; set; }
+    }
+}
